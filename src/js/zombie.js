159146259db0bb61e0d5e17250b65c57bcd6f6c1
AngryPapayah <airissavermo@hotmail.com>
2025-05-22 10:46:26 +0200

    zombie spawn loop gemaakt

--- src/js/zombie.js
+++ src/js/zombie.js
@@ -3,11 +3,22 @@ import { Resources } from './resources.js';
 
 export class Zombie extends Actor {
     onInitialize() {
+        this.zombieRandomPosition();
         this.graphics.use(Resources.Zombie.toSprite())
-        this.pos = new Vector(600, 500)
+        
+        this.events.on("exitviewport", (e) => this.zombieRandomPosition(e))
+        
+        
+    }
+
+    zombieRandomPosition() {
+        this.pos = new Vector(Math.random() * 800, Math.random() * 600);
+        this.vel = new Vector(Math.random() * 100, Math.random() * 100);
         this.scale = new Vector(0.52, 0.52)
-        
-        
+
+        if (this.vel.x > 0) {
+            this.graphics.flipHorizontal = true;
+        }
     }
 
 }
