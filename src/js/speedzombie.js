1402ca71671d646c33d3b1320e49119e1b5c798f
AngryPapayah <airissavermo@hotmail.com>
2025-06-01 03:39:19 +0200

    } vergeten

--- src/js/speedzombie.js
+++ src/js/speedzombie.js
@@ -8,4 +8,5 @@ export class SpeedZombie extends Zombie {
         this.speed = 150 + Math.random() * 50; // Maak deze zombie sneller
         this.graphics.use(Resources.Speedzombie.toSprite()); 
         this.scale = new Vector(0.05, 0.05); 
+    }
 }
