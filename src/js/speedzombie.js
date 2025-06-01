d510f008de73803103954cc486ca0ace37dde64c
AngryPapayah <airissavermo@hotmail.com>
2025-06-01 02:31:00 +0200

    kleine aanpassing

--- src/js/speedzombie.js
+++ src/js/speedzombie.js
@@ -4,9 +4,8 @@ import { Zombie } from "./zombie";
 
 export class SpeedZombie extends Zombie {
     constructor(shooter) {
-        super(shooter); // Roep de constructor van Zombie aan
+        super(shooter); 
         this.speed = 150 + Math.random() * 50; // Maak deze zombie sneller
-        this.graphics.use(Resources.Speedzombie.toSprite()); // Gebruik een ander plaatje
-        this.scale = new Vector(0.05, 0.05); // Optioneel: pas schaal aan
-    }
+        this.graphics.use(Resources.Speedzombie.toSprite()); 
+        this.scale = new Vector(0.05, 0.05); 
 }
